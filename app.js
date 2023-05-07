bb3d69ea5af2d2cef69ce7aa2ea5e9319e7709cf
Debagnik Sen <97969319+debagnik02@users.noreply.github.com>
2023-05-07 19:38:24 +0530

    Added offset functionality in JS file

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 let form=document.querySelector('#searchForm');
 let ytmusic=document.querySelector('#ytresults');
 var API_KEY="AIzaSyBZRMwtW2H0QeI2xhIJ6bVu_8ySfymXXoM";
-
+let resultsection=document.querySelector("#results");
 
 form.addEventListener('submit',(e)=>{
-    window.location.href="#results-section"
     e.preventDefault();
     let inputValue=document.querySelector('#searchInput').value;
-    youtubeResults(inputValue);
-    spotifyResults(inputValue);
-    soundcloudResults(inputValue);
+    let ytoffset=document.querySelector('#ytoffset').value;
+    let spotifyoffset=document.querySelector('#spotifyoffset').value;
+    let soundcloudoffset=document.querySelector('#soundcloudoffset').value;
+    if(inputValue.length)
+    {
+    resultsection.style.display="block";
+    window.location.href="#results-section"
+    youtubeResults(inputValue,ytoffset);
+    spotifyResults(inputValue,spotifyoffset);
+    soundcloudResults(inputValue,soundcloudoffset);
+    }
 })
 
-
-let youtubeResults=(inputValue)=>{
+let youtubeResults=(inputValue,offset)=>{
+    let p=1
     while(ytmusic.lastChild)
     {
         ytmusic.removeChild(ytmusic.lastChild)
     }
-    let link=`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${inputValue+" song"}&maxResults=6&type=video&key=AIzaSyDrakPogPsEqtniLjK4MPkimLRUGsObAXQ`
+    let link=`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${inputValue+" song"}&maxResults=30&type=video&key=AIzaSyDrakPogPsEqtniLjK4MPkimLRUGsObAXQ`
     fetch(link)
     .then(res=>{
        res.json().then((data)=>{
            for (let item of data.items)
-           {
+           {    
+               if(p>((offset-1)*6)){
+                console.log("YOUTUUBE DISPLAY")
+               if(p>(offset*6))
+                    break;
                let ytvid=document.createElement('iframe');
                ytvid.style.width="420";
                ytvid.style.height="315";
@@ -32,6 +43,8 @@ let youtubeResults=(inputValue)=>{
                ytvid.style.frameborder="0";
                ytvid.allowFullscreen=true;
                ytmusic.append(ytvid)
+               }
+               p+=1;
            }
        })
     })
@@ -49,6 +62,8 @@ let clientSecret="f0124445e282402ca423b42ad91ec0bb";
 let access_token='';
 
 let access=async ()=>{
+    resultsection.style.display="none";
+    window.location.href="#search-section"
     let res=await fetch("https://accounts.spotify.com/api/token",{
 
         method:"POST",
@@ -62,13 +77,13 @@ let access=async ()=>{
     access_token=await res.json();
     access_token=access_token.access_token;
 }
-let spotifyResults=async (value)=>{
+let spotifyResults=async (value,offset)=>{
     while(spotifymusic.lastChild)
     {
         spotifymusic.removeChild(spotifymusic.lastChild)
     }
     try{
-    let res=await fetch(`https://api.spotify.com/v1/search?q=${value}&type=track&market=IN&limit=7&offset=1`, {
+    let res=await fetch(`https://api.spotify.com/v1/search?q=${value}&type=track&market=IN&limit=7&offset=${offset}`, {
         headers:{
             "Authorization": `Bearer ${access_token}`
         }
@@ -79,7 +94,7 @@ let spotifyResults=async (value)=>{
         let songId=((""+item.uri).substring(14));
         let song=document.createElement('iframe');
         song.src="https://open.spotify.com/embed/track/"+songId;
-        song.style.width="315";
+        song.style.width="320";
         song.style.height="395";
         song.style.frameBorder="0"
         song.allowFullscreen="";
@@ -96,12 +111,12 @@ catch(e){
 
 
 let soundcloudresults=document.querySelector('#soundcloudresults');
-let soundcloudResults=async (value)=>{
+let soundcloudResults=async (value,offset)=>{
 
     while(soundcloudresults.lastChild){
         soundcloudresults.removeChild(soundcloudresults.lastChild)
     }
-    let res=await fetch(`https://soundcloud-scraper.p.rapidapi.com/v1/search/tracks?term=${value}&limit=6`,{
+    let res=await fetch(`https://soundcloud-scraper.p.rapidapi.com/v1/search/tracks?term=${value}&limit=${6}&offset=${(offset-1)*6}`,{
 
     headers:{
         "X-RapidAPI-Key":"605f24bd7fmshdf81bc20da8d50ep171456jsn63f75bd3db92",
